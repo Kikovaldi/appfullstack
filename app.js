af59556109bd9733f40c9dce8c63a5978d098629
Kikovaldi <franciscovaldiviaclavijo@gmail.com>
2023-10-11 09:51:51 +0200

    planitlla

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 2018
+app.listen(port, () => {
+    console.log (`listening at port ${port}`)
+})
+
+app.get("/", (req, res)=> {
+    res.status(200).send("<h1>Proyecto Final</h1>")
+})
